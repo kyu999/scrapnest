66ab045012f74d2170c00b9aa85ac63a514e817c
kyu999 <kyukokkyou999@gmail.com>
2019-06-18 17:01:13 +0900

    sample data追加

---
+++ output_data_sample.js
@@ -0,0 +1,210 @@
+var graph = {
+    "page_infos": [
+        {
+            "updated": 1560824089, 
+            "created": 1560824089, 
+            "#lines": 3, 
+            "links": [], 
+            "title": "Action4"
+        }, 
+        {
+            "updated": 1560824084, 
+            "created": 1560824083, 
+            "#lines": 3, 
+            "links": [], 
+            "title": "Action3"
+        }, 
+        {
+            "updated": 1560824077, 
+            "created": 1560824077, 
+            "#lines": 3, 
+            "links": [], 
+            "title": "Further Action4"
+        }, 
+        {
+            "updated": 1560824072, 
+            "created": 1560824069, 
+            "#lines": 3, 
+            "links": [], 
+            "title": "Further Action3"
+        }, 
+        {
+            "updated": 1560823097, 
+            "created": 1560823077, 
+            "#lines": 5, 
+            "links": [
+                "Further Action3", 
+                "Further Action4"
+            ], 
+            "title": "Action2"
+        }, 
+        {
+            "updated": 1560823072, 
+            "created": 1560823032, 
+            "#lines": 4, 
+            "links": [
+                "Further Action1", 
+                "Further Action2"
+            ], 
+            "title": "Action1"
+        }, 
+        {
+            "updated": 1560823027, 
+            "created": 1560822995, 
+            "#lines": 6, 
+            "links": [
+                "Action1", 
+                "Action2", 
+                "Action3", 
+                "Action4"
+            ], 
+            "title": "My TODOs"
+        }, 
+        {
+            "updated": 1560822974, 
+            "created": 1560822974, 
+            "#lines": 46, 
+            "links": [
+                " ", 
+                "\u5225\u30da\u30fc\u30b8", 
+                "\u30ea\u30f3\u30af", 
+                " ", 
+                "/help-jp/\u8a18\u6cd5", 
+                "Scrapbox", 
+                "\u30ea\u30f3\u30af", 
+                "\u3064\u306a\u304c\u308a", 
+                "\u8aad\u3093\u3060\u672c", 
+                "\u4eba\u7269", 
+                "\u30bf\u30b9\u30af", 
+                "\u81ea\u7531", 
+                "/help-jp/", 
+                "/help/", 
+                "\u304a\u554f\u3044\u5408\u308f\u305b https://scrapbox.io/contact", 
+                "/forum-jp", 
+                "/help-jp/\u30b5\u30f3\u30d7\u30eb", 
+                "/nota-private-sample", 
+                "\u4f01\u696d\u306e\u5229\u7528\u4e8b\u4f8b\u3092\u63b2\u8f09\u3057\u3066\u3044\u307e\u3059 https://scrapbox.io/business", 
+                "Scrapbox\u306e\u4f01\u696d\u3067\u306e\u4f7f\u3044\u9053 https://scrapbox.io/business/how-it-works"
+            ], 
+            "title": "Scrapbox\u306e\u4f7f\u3044\u65b9"
+        }
+    ], 
+    "nodes": [
+        {
+            "score": 0, 
+            "type": "circle", 
+            "id": "Action4", 
+            "links": [], 
+            "size": 0
+        }, 
+        {
+            "score": 0, 
+            "type": "circle", 
+            "id": "Action3", 
+            "links": [], 
+            "size": 0
+        }, 
+        {
+            "score": 0, 
+            "type": "circle", 
+            "id": "Further Action4", 
+            "links": [], 
+            "size": 0
+        }, 
+        {
+            "score": 0, 
+            "type": "circle", 
+            "id": "Further Action3", 
+            "links": [], 
+            "size": 0
+        }, 
+        {
+            "score": 2, 
+            "type": "circle", 
+            "id": "Action2", 
+            "links": [
+                "Further Action3", 
+                "Further Action4"
+            ], 
+            "size": 2
+        }, 
+        {
+            "score": 2, 
+            "type": "circle", 
+            "id": "Action1", 
+            "links": [
+                "Further Action1", 
+                "Further Action2"
+            ], 
+            "size": 2
+        }, 
+        {
+            "score": 4, 
+            "type": "circle", 
+            "id": "My TODOs", 
+            "links": [
+                "Action1", 
+                "Action2", 
+                "Action3", 
+                "Action4"
+            ], 
+            "size": 4
+        }, 
+        {
+            "score": 20, 
+            "type": "circle", 
+            "id": "Scrapbox\u306e\u4f7f\u3044\u65b9", 
+            "links": [
+                " ", 
+                "\u5225\u30da\u30fc\u30b8", 
+                "\u30ea\u30f3\u30af", 
+                " ", 
+                "/help-jp/\u8a18\u6cd5", 
+                "Scrapbox", 
+                "\u30ea\u30f3\u30af", 
+                "\u3064\u306a\u304c\u308a", 
+                "\u8aad\u3093\u3060\u672c", 
+                "\u4eba\u7269", 
+                "\u30bf\u30b9\u30af", 
+                "\u81ea\u7531", 
+                "/help-jp/", 
+                "/help/", 
+                "\u304a\u554f\u3044\u5408\u308f\u305b https://scrapbox.io/contact", 
+                "/forum-jp", 
+                "/help-jp/\u30b5\u30f3\u30d7\u30eb", 
+                "/nota-private-sample", 
+                "\u4f01\u696d\u306e\u5229\u7528\u4e8b\u4f8b\u3092\u63b2\u8f09\u3057\u3066\u3044\u307e\u3059 https://scrapbox.io/business", 
+                "Scrapbox\u306e\u4f01\u696d\u3067\u306e\u4f7f\u3044\u9053 https://scrapbox.io/business/how-it-works"
+            ], 
+            "size": 20
+        }
+    ], 
+    "project_name": "name", 
+    "exported": 1560824106, 
+    "links": [
+        {
+            "source": 4, 
+            "target": 3
+        }, 
+        {
+            "source": 4, 
+            "target": 2
+        }, 
+        {
+            "source": 6, 
+            "target": 5
+        }, 
+        {
+            "source": 6, 
+            "target": 4
+        }, 
+        {
+            "source": 6, 
+            "target": 1
+        }, 
+        {
+            "source": 6, 
+            "target": 0
+        }
+    ]
+}
